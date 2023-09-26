df6b176b30a5dd427963ad5d840d3a88c7bafa4b
BootCamp_Xolo <141790916+ocarly@users.noreply.github.com>
2023-09-25 22:42:31 -0500

    saveBtn added and local storage steeze

--- script.js
+++ script.js
@@ -3,6 +3,15 @@ $(function(){
     // function for date and time using day.js
     $("#currentDay").text(dayjs().format("ddd, MMM DD, YYYY h:mm a"));
 
-
+    $(`.saveBtn`).on("click", function () {
+        const id=$(this).parent().attr("id")
+        const stuffToDo=$(this).siblings(".description").val()
+        localStorage.setItem(id,stuffToDo)
+        
+      })
+     
+  for (let i = 9; i <=17;i++){
+    $(`#${i} .description`).val(localStorage.getItem(`${i}`))
+  }
 
 })
